c18c462dbeba51e1ee2da781405bea58c2f0e7d0
FabianM <luiisfabian96@gmail.com>
2023-12-14 00:10:03 -0500

    card de DC y marvel terminados con las imagenes arregladas

--- src/heroes/components/HeroList.jsx
+++ src/heroes/components/HeroList.jsx
@@ -1,20 +1,19 @@
 import { getHeroesByPublisher } from "../helpers"
+import { HeroCard } from "./HeroCard"
 
 export const HeroList = ({ publisher }) => {
 
     const heroes = getHeroesByPublisher(publisher)
     const heroNames = heroes.map(hero => (
-        <li key={hero.id}>
-            {hero.superhero}
-        </li>
+        <HeroCard key={hero.id} {...hero} />
     ))
 
     return (
         <>
-            <ul>
+            <div className="row rows-cols-1 row-cols-md-3 g-3">
                 {heroNames}
 
-            </ul>
+            </div>
 
         </>
     )
